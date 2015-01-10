6ca21ea4ee0a43eb1830359865c534be462e1a04
Brady Somerville <brady.somerville@gmail.com>
2015-01-10 14:36:01 -0600

    Track 'customer added product to wishlist' action
    oops

---
+++ app/controllers/spree/wished_products_controller_decorator.rb
@@ -0,0 +1,14 @@
+if defined? Spree::WishedProductsController
+  Spree::WishedProductsController.class_eval do
+    after_filter :betaout_track_product_added_to_wishlist, only: :create
+
+    private
+
+      def betaout_track_product_added_to_wishlist
+        Betaout.customer_added_product_to_wishlist({
+          session: session,
+          product: @wished_product.variant.product,
+        })
+      end
+  end
+end
--- lib/betaout/api.rb
+++ lib/betaout/api.rb
@@ -66,6 +66,18 @@ module Betaout
       })
     end
 
+    def customer_added_product_to_wishlist(email, product)
+      body_params = @body_params.merge({
+        'email' => email ? email : '',
+        'action' => 'wishlist',
+        'products' => [ product ],
+      })
+
+      self.class.post("/v1/user/customer_activity", {
+        body: 'params=' + body_params.to_json,
+      })
+    end
+
     # TODO: does this work? should I expect to see the product in the Betaout admin if no one's viewed it or purchased it yet?
     def product_added(product)
       self.class.post("/v1/product/add", body: @body_params.merge(product).to_json)
@@ -97,6 +109,12 @@ module Betaout
     product_hash = Product.new(args).to_hash
     # TODO: add email if/when we have it
     API.new(args[:session]).customer_removed_product_from_cart(nil, product_hash, args[:quantity], args[:order])
+  end
+
+  def self.customer_added_product_to_wishlist(args)
+    product_hash = Product.new(args).to_hash
+    # TODO: add email if/when we have it
+    API.new(args[:session]).customer_added_product_to_wishlist(nil, product_hash)
   end
 
   def self.product_added(args)
